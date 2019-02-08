d83ee9c1dbd86eb892e0a70040feb233fc6b28f1
Morten Nielsen <morten@klarity.org>
2019-02-08 12:51:24 +0100

    fixing the blog listing

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
 		<?php
 		if ( have_posts() ) :
 
@@ -29,8 +28,12 @@ get_header();
 				<?php
 			endif;
 
+			?>
+			<div class="container">
+			<?php	
 			/* Start the Loop */
 			while ( have_posts() ) :
+				
 				the_post();
 
 				/*
@@ -38,11 +41,14 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/post-list', get_post_type() );
 
 			endwhile;
 
 			the_posts_navigation();
+			?>
+			</div>
+			<?php
 
 		else :
 
--- single.php
+++ single.php
@@ -15,6 +15,7 @@ get_header();
 
 		<?php
 		while ( have_posts() ) :
+			
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
