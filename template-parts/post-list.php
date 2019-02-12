4d26a14698aa96775f47f5ddfb87b13a11d7a573
Morten Nielsen <morten@klarity.org>
2019-02-12 13:29:18 +0100

    search results and blogposts now follow the same listing style

--- template-parts/post-list.php
+++ template-parts/post-list.php
@@ -11,20 +11,23 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-<div class="col s12">
+<div class="col s12" style="margin-top: 24px;">
    <div class="card horizontal">
-      <div class="card-image" style="margin: 10px;">
-        <?php the_post_thumbnail('thumbnail'); ?>
-      </div>
+
       <div class="card-stacked">
         <div class="card-content">
-        <div class="entry-meta left-align">
             <?php
-            klarity_posted_on();
-            klarity_posted_by();
-            ?>
-        </div>
-        <h3 class="header left-align"> <?php the_title( ) ?></h3>
+
+              if ( 'post' === get_post_type() ) :
+                ?>
+                  <div class="entry-meta left-align hide-on-med-and-down shade">
+                  <?php
+                    klarity_posted_on();
+                    klarity_posted_by();
+                  ?>
+                </div><!-- .entry-meta -->
+            <?php endif; ?>
+        <h3 class="header left-align" style="margin-top: 0px; margin-bottom: 0px;"> <?php the_title( ) ?></h3>
         <?php
         	if ( 'post' === get_post_type() ) :
                 ?>
