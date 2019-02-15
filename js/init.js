646008f016ce33309933a81cc8224386e7f3d632
Bruno Perel <bruno@klarity.org>
2019-02-15 10:46:57 +0100

    Force comment form validation

--- js/init.js
+++ js/init.js
@@ -1,7 +1,10 @@
 (function($){
   $(function(){
-
+    // Init Materialize's parallax components
     $('.parallax').parallax();
+
+    // Force comment form validation (disabled by default on html5 forms, see comment-template.php)
+    $('#commentform').removeAttr('novalidate');
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
