8cc5017b7419bbd25e942c62001111fe6354998e
Jonas Alfthan <jonasalfthan@MacBook-Pro-8.local>
2019-02-18 13:49:57 +0100

    Removing unnessessary content

--- 404.php
+++ 404.php
@@ -10,51 +10,19 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'klarity' ); ?></h1>
-				</header><!-- .page-header -->
+      <section class="error-404 not-found">
+        <div class="page-content entry-content">
+          <h2 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'klarity' ); ?></h2>
+          <p><a href="/">Go Home</a></p>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'klarity' ); ?></p>
+        </div><!-- .page-content -->
+      </section><!-- .error-404 -->
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'klarity' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$klarity_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'klarity' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$klarity_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    </main><!-- #main -->
+  </div><!-- #primary -->
 
 <?php
 get_footer();
