b75b0705fcced7dbea20ef3031ea2e7a831a4406
Bruno Perel <bruno@klarity.org>
2019-03-25 15:00:18 +0100

    Fix escaped tags in blog header

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -24,16 +24,14 @@ if ( ! function_exists( 'klarity_posted_on' ) ) :
 			esc_html( get_the_modified_date() )
 		);
 
-		echo '<span class="posted-on">'.
-      esc_html(
-        sprintf(
-        /* translators: %s: post date. */
-          esc_html_x( 'Posted on %s', 'post date', 'klarity' ),
-          '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-        ),
-        'klarity'
-      )
-    .'</span>';
+
+	  $posted_on = sprintf(
+	  /* translators: %s: post date. */
+		  esc_html_x( 'Posted on %s', 'post date', 'klarity' ),
+		  '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	  );
+
+	  echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 	}
 endif;
@@ -44,15 +42,12 @@ if ( ! function_exists( 'klarity_posted_by' ) ) :
 	 */
 	function klarity_posted_by() {
 		echo '<span class="byline"> '.
-      esc_html(
-        sprintf(
-        /* translators: %s: post author. */
-          esc_html_x( 'by %s', 'post author', 'klarity' ),
-          '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-        ),
-        'klarity'
+      sprintf(
+      /* translators: %s: post author. */
+        esc_html_x( 'by %s', 'post author', 'klarity' ),
+        '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
       )
-    .'</span>';
+  .'</span>';
 
 	}
 endif;
@@ -67,15 +62,13 @@ if ( ! function_exists( 'klarity_entry_footer' ) ) :
 			/* translators: used between list items, there is a space after the comma */
 			$categories_list = get_the_category_list( esc_html__( ', ', 'klarity' ) );
 			if ( $categories_list ) {
-				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( ' in %1$s', 'klarity' ) . '</span>', esc_html($categories_list)); // WPCS: XSS OK.
+		    printf( '<span class="cat-links">' . esc_html__( ' in %1$s', 'klarity' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 			}
 
 			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'klarity' ) );
 			if ( $tags_list ) {
-				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'klarity' ) . '</span>', esc_html($tags_list )); // WPCS: XSS OK.
+		    printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'klarity' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
 
