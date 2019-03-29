b5e6bf1e7c905a98ac98a811fab8503efc5756c6
Bruno Perel <bruno@klarity.org>
2019-03-29 12:11:12 +0100

    Handle sub-menus

--- js/navigation.js
+++ js/navigation.js
@@ -1,129 +1,49 @@
-/**
- * File navigation.js.
- *
- * Handles toggling the navigation menu for small screens and enables TAB key
- * navigation support for dropdown menus.
- */
-( function() {
-	var container, button, menu, links, i, len;
-
-	container = document.getElementById( 'site-navigation' );
-	if ( ! container ) {
-		return;
-	}
-
-	button = container.getElementsByTagName( 'button' )[0];
-	if ( 'undefined' === typeof button ) {
-		return;
-	}
-
-	menu = container.getElementsByTagName( 'ul' )[0];
-
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
-
-	menu.setAttribute( 'aria-expanded', 'false' );
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
-	}
-
-	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-			container.className = container.className.replace( ' toggled', '' );
-			button.setAttribute( 'aria-expanded', 'false' );
-			menu.setAttribute( 'aria-expanded', 'false' );
-		} else {
-			container.className += ' toggled';
-			button.setAttribute( 'aria-expanded', 'true' );
-			menu.setAttribute( 'aria-expanded', 'true' );
-		}
-	};
-
-	// Get all the link elements within the menu.
-	links    = menu.getElementsByTagName( 'a' );
-
-	// Each time a menu link is focused or blurred, toggle focus.
-	for ( i = 0, len = links.length; i < len; i++ ) {
-		links[i].addEventListener( 'focus', toggleFocus, true );
-		links[i].addEventListener( 'blur', toggleFocus, true );
-	}
-
-	/**
-	 * Sets or removes .focus class on an element.
-	 */
-	function toggleFocus() {
-		var self = this;
-
-		// Move up through the ancestors of the current link until we hit .nav-menu.
-		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
-
-			// On li elements toggle the class .focus.
-			if ( 'li' === self.tagName.toLowerCase() ) {
-				if ( -1 !== self.className.indexOf( 'focus' ) ) {
-					self.className = self.className.replace( ' focus', '' );
-				} else {
-					self.className += ' focus';
-				}
-			}
-
-			self = self.parentElement;
-		}
-	}
-
-	/**
-	 * Toggles `focus` class to allow submenu access on tablets.
-	 */
-	( function( container ) {
-		var touchStartFn, i,
-			parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
-
-		if ( 'ontouchstart' in window ) {
-			touchStartFn = function( e ) {
-				var menuItem = this.parentNode, i;
-
-				if ( ! menuItem.classList.contains( 'focus' ) ) {
-					e.preventDefault();
-					for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
-						if ( menuItem === menuItem.parentNode.children[i] ) {
-							continue;
-						}
-						menuItem.parentNode.children[i].classList.remove( 'focus' );
-					}
-					menuItem.classList.add( 'focus' );
-				} else {
-					menuItem.classList.remove( 'focus' );
-				}
-			};
-
-			for ( i = 0; i < parentLink.length; ++i ) {
-				parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
-			}
-		}
-	}( container ) );
-} )();
-
 (function($) {
-	$(document).ready(function() {
-		$('.menu-item-has-children > a').click(function(e) {
-			e.preventDefault();
-		});
-		var hoverOrClick = function () {
-			var submenu = $(this).children('.sub-menu');
-			var submenuHeight = $(submenu).height();
-			if ( $(submenu).is(':hidden') ) {
-				$(submenu).slideDown(200);
-				$(this).animate({ "height": "+="+submenuHeight+"px" }, 200, function() { $(this).height(45 + submenuHeight)} );
-			} else {
-				$(submenu).slideUp(200);
-				$(this).animate({ "height": "-="+submenuHeight+"px" }, 200, function() { $(this).height(45)} );
+	$('.menu-item-has-children > a').click(function(e) {
+		e.preventDefault();
+	});
+
+	function showSubMenu(element) {
+		if (!$(element).hasClass('animating')) {
+			const submenu = $(element).children('.sub-menu');
+			if (submenu.is(':hidden')) {
+				const submenuHeight = submenu.height();
+				$(element).addClass('animating expanded');
+				submenu.slideDown(200);
+				$(element).animate({"height": "+=" + submenuHeight + "px"}, 200, function () {
+					$(element).height(45 + submenuHeight).removeClass('animating');
+				});
 			}
 		}
-		$('.nav-wrapper li > .sub-menu').parent().hover(hoverOrClick);
-		$('.nav-wrapper li > .sub-menu').hide();
-	});
+	}
+
+	function hideSubMenu(element) {
+		if (!$(element).hasClass('animating')) {
+			const submenu = $(element).children('.sub-menu');
+			if (!submenu.is(':hidden')) {
+				const submenuHeight = submenu.height();
+				$(element).addClass('animating').removeClass('expanded');
+				submenu.slideUp(200);
+				$(element).animate({"height": "-=" + submenuHeight + "px"}, 200, function () {
+					$(element).height(45).removeClass('animating');
+				});
+			}
+		}
+	}
+
+	$('.nav-wrapper li > .sub-menu').parent()
+		.on('click', function (e) {
+			if ($(this).children('.sub-menu').is(':hidden')) {
+				showSubMenu(this);
+			} else {
+				hideSubMenu(this);
+			}
+			e.stopPropagation();
+			e.preventDefault();
+		})
+		.on('mouseleave', function() { hideSubMenu($(this));})
+		.on('mouseenter', function() { showSubMenu($(this));});
+	$('.nav-wrapper li > .sub-menu').hide();
 })( jQuery );
 
 (function($){
