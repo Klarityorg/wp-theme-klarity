c0e65f87712728cec0c0ec301a3a4bf6dc519ad5
Bruno Perel <bruno@klarity.org>
2019-04-02 10:32:02 +0200

    Remove PHP 7-only code

--- template-parts/post-list.php
+++ template-parts/post-list.php
@@ -14,7 +14,18 @@
     $content = wp_trim_words($post->post_content, $num_words = 100 );
     $formated_date = get_the_date( get_option( 'date_format' ), $post);
     preg_match('/videoThumbnail":"(.+)"/', $post->post_content, $matches);
-    $image = $matches[1] ?? wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail')[0] ?? '';
+    if (!is_null($matches[1])) {
+		  $image = $matches[1];
+    }
+    else {
+      $attachmentImage = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail')[0];
+      if (!is_null($attachmentImage)) {
+		    $image = $attachmentImage;
+      }
+      else {
+        $image = '';
+      }
+    }
  ?>
  <div class="post-list-container">
     <a href="<?php echo esc_html(get_permalink($post)) ?>">
